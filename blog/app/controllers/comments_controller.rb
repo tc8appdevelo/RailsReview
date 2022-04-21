51d03e8022cbf0fbbe22029d6dc46a41e4b02f83
tc8appdevelo <tc8appdevelo@gmail.com>
2022-04-20 20:24:43 -0500

    added basic authintication

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "ahh",
+                               password: "secret",
+                               only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
